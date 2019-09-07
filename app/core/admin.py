cb905d67401c3ecbf0b0b83a253f459dd48f3be2
aimee <zvok59@gmail.com>
2019-09-07 15:18:44 -0500

    adding endpoint for creating and authenticating users

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,9 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
+
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
@@ -14,7 +16,7 @@ class UserAdmin(BaseUserAdmin):
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
 
         ),
-        (_('Important dates'), {'fields':('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
@@ -22,5 +24,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
